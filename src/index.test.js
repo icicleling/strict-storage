4de866b9413ff32f77f5e71fdf08c966e76e4be8
icicle <unknown@icicleling.com>
2021-08-15 00:34:55 +0800

    feat: storage

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,14 @@
-import foo from ".";
+import getStrictStorage from "./index";
 
-test("test init", () => {
-  expect(foo()).toBe("hey");
+test("check the key of the returned object", () => {
+  const storage = getStrictStorage();
+  expect(storage).toHaveProperty("strictLocalStorage");
+  expect(storage).toHaveProperty("strictSessionStorage");
+
+  expect(storage.strictLocalStorage).toHaveProperty("setItem");
+  expect(storage.strictLocalStorage).toHaveProperty("getItem");
+  expect(storage.strictLocalStorage).toHaveProperty("removeItem");
+  expect(storage.strictLocalStorage).toHaveProperty("key");
+  expect(storage.strictLocalStorage).toHaveProperty("clear");
+  expect(storage.strictLocalStorage).toHaveProperty("length");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
-const foo = (): string => "hey";
-export default foo;
+const getStrictStorage = <T extends object>() => {
+  const getStorage = (storage: Storage) => ({
+    setItem<K extends Extract<keyof T, string>, V extends T[K]>(
+      field: K,
+      value: V
+    ) {
+      storage.setItem(field, JSON.stringify({ value }));
+    },
+    getItem<K extends Extract<keyof T, string>, V extends T[K]>(
+      field: K
+    ): V | null {
+      const jsonValue = storage.getItem(field);
+      if (!jsonValue) return null;
+
+      return JSON.parse(jsonValue).value;
+    },
+    removeItem<K extends Extract<keyof T, string>>(field: K) {
+      storage.removeItem(field);
+    },
+    key(index: number) {
+      storage.key(index);
+    },
+    clear() {
+      storage.clear();
+    },
+    length: storage.length,
+  });
+
+  return {
+    strictLocalStorage: getStorage(localStorage),
+    strictSessionStorage: getStorage(sessionStorage),
+  };
+};
+
+export default getStrictStorage;
